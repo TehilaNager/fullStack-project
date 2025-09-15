4049013471efd173ee45cfec35bb751bc4497336
TehilaNager <tehila6619366@gmail.com>
2025-09-15 11:54:48 +0300

    update user

--- backend/routes/user_route.js
+++ backend/routes/user_route.js
@@ -25,6 +25,31 @@ router.get("/:id", authMW, async (req, res) => {
     res.json(user);
 });
 
-router.put("/:id", authMW, async (req, res) => { });
+router.put("/:id", authMW, async (req, res) => {
+    const { error, value } = validateUpdate.validate(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    const isUser = req.params.id === req.user._id;
+    const isAdmin = req.user.isAdmin;
+
+    if (!isAdmin && !isUser) {
+        res.status(400).send("Access denied. You can only update your own profile or you must be an admin to update others' profiles.");
+        return;
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, value, { new: true });
+
+    if (!updatedUser) {
+        res.status(400).send("User not found");
+        return;
+    }
+
+    const filteredUser = _.pick(updatedUser, ["_id", "fullName", "email", "isAdmin", "phone", "city", "favoriteRequests", "favoriteOffers", "createdAt", "updatedAt"]);
+
+    res.json(filteredUser);
+});
 
 module.exports = router;
