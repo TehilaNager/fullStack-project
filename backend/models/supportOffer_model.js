906708b45373c86bf886cb68364618edb293490c
TehilaNager <tehila6619366@gmail.com>
2025-09-15 18:56:18 +0300

    create validate to model supportOffer

--- backend/models/supportOffer_model.js
+++ backend/models/supportOffer_model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Joi = require("joi");
 
 const supportOfferSchema = new mongoose.Schema({
     supporter: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
@@ -16,4 +17,36 @@ const supportOfferSchema = new mongoose.Schema({
     status: { type: String, enum: ['פתוחה', 'הושלמה'], default: 'פתוחה' }
 }, { timestamps: true });
 
-module.exports = mongoose.model('SupportOffer', supportOfferSchema);
+const Offer = mongoose.model('SupportOffer', supportOfferSchema);
+
+const validateOffer = Joi.object({
+    title: Joi.string().min(2).max(256).required(),
+    description: Joi.string().min(5).max(1024).required(),
+    category: Joi.string()
+        .valid('ציוד', 'אוכל ושתייה', 'תחבורה והסעות', 'סיוע כספי', 'תמיכה נפשית וחברתית', 'שירותים ולוגיסטיקה', 'בריאות ורפואה', 'הוואי ומורל', 'אחר')
+        .required(),
+    location: Joi.object({
+        region: Joi.string().valid('צפון', 'מרכז', 'דרום').required(),
+        city: Joi.string().min(2).max(256).required()
+    }).required(),
+    status: Joi.string().valid('פתוחה', 'הושלמה').optional()
+});
+
+const validateOfferUpdate = Joi.object({
+    title: Joi.string().min(2).max(256).optional(),
+    description: Joi.string().min(5).max(1024).optional(),
+    category: Joi.string()
+        .valid('ציוד', 'אוכל ושתייה', 'תחבורה והסעות', 'סיוע כספי', 'תמיכה נפשית וחברתית', 'שירותים ולוגיסטיקה', 'בריאות ורפואה', 'הוואי ומורל', 'אחר')
+        .optional(),
+    location: Joi.object({
+        region: Joi.string().valid('צפון', 'מרכז', 'דרום').optional(),
+        city: Joi.string().min(2).max(256).optional()
+    }).optional(),
+    status: Joi.string().valid('פתוחה', 'הושלמה').optional()
+}).min(1);
+
+module.exports = {
+    Offer,
+    validateOffer,
+    validateOfferUpdate
+}
