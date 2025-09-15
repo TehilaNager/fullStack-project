20e42fbd75e90c121c7dd97131aa552ca4494c5c
TehilaNager <tehila6619366@gmail.com>
2025-09-15 21:23:45 +0300

    add logic in file favorites_route

--- backend/routes/favorites_route.js
+++ backend/routes/favorites_route.js
@@ -1,6 +1,97 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
-router.post("/", (req, res) => { });
+const { User } = require("../models/user_model");
+const { Request } = require("../models/supportRequest_model");
+const { Offer } = require("../models/supportOffer_model");
+
+const authMW = require("../middleware/auth");
+
+router.patch("/request/:id", authMW, async (req, res) => {
+    const requestId = req.params.id;
+
+    if (!mongoose.isValidObjectId(requestId)) {
+        res.status(400).send("The value provided is not a valid ObjectId. Please return a valid MongoDB ObjectId (a 24-character hexadecimal string).");
+        return;
+    }
+
+    const request = await Request.findById(requestId);
+
+    if (!request) {
+        res.status(400).send("Request not found. Please provide a valid request ID.");
+        return;
+    }
+
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+        res.status(400).send("User not found.");
+        return;
+    }
+
+    if (user.favoriteRequests.includes(requestId)) {
+        const index = user.favoriteRequests.indexOf(requestId);
+        user.favoriteRequests.splice(index, 1);
+    } else {
+        user.favoriteRequests.push(requestId);
+    }
+
+    await user.save();
+
+    const filteredUser = _.pick(user, ["_id", "fullName", "email", "isAdmin", "phone", "city", "favoriteRequests", "favoriteOffers", "createdAt", "updatedAt"])
+
+    res.json(filteredUser);
+});
+
+router.patch("/offer/:id", authMW, async (req, res) => {
+    const offerId = req.params.id;
+
+    if (!mongoose.isValidObjectId(offerId)) {
+        res.status(400).send("The value provided is not a valid ObjectId. Please return a valid MongoDB ObjectId (a 24-character hexadecimal string).");
+        return;
+    }
+
+    const offer = await Offer.findById(offerId);
+
+    if (!offer) {
+        res.status(400).send("Offer not found. Please provide a valid offer ID.");
+        return;
+    }
+
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+        res.status(400).send("User not found.");
+        return;
+    }
+
+    if (user.favoriteOffers.includes(offerId)) {
+        const index = user.favoriteOffers.indexOf(offerId);
+        user.favoriteOffers.splice(index, 1);
+    } else {
+        user.favoriteOffers.push(offerId);
+    }
+
+    await user.save();
+
+    const filteredUser = _.pick(user, ["_id", "fullName", "email", "isAdmin", "phone", "city", "favoriteRequests", "favoriteOffers", "createdAt", "updatedAt"])
+
+    res.json(filteredUser);
+});
+
+router.get("/", authMW, authMW, async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+        res.status(400).send("User not found.");
+        return;
+    }
+
+    const filteredUser = _.pick(user, ["_id", "fullName", "favoriteRequests", "favoriteOffers"]);
+
+    res.json(filteredUser);
+});
 
 module.exports = router;
